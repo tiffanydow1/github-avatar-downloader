bf4ebee29c42c753bfb987a4fd1654ba73ca5ff2
Tiffany <tiffanyjdow@gmail.com>
2018-06-11 20:28:22 +0000

    added authorization key to header

--- download_avatars.js
+++ download_avatars.js
@@ -4,6 +4,17 @@ console.log("Welcome to the GitHub Avatar Downloader");
 
 function getRepoContributors(repoOwner, repoName, cb) {
 
+  var options = {
+    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+    headers: {
+      'User-Agent': 'request',
+      'Authorization': '560c59f9fbda46b5fabc0c37641d3e6845e2cf0e'
+    }
+  };
+
+  request(options, function(err, res, body) {
+    cb(err, body);
+  });
 }
 
 getRepoContributors("jquery", "jquery", function(err, result) {
@@ -12,3 +23,25 @@ getRepoContributors("jquery", "jquery", function(err, result) {
 });
 //function to use request lib to fetch list of contributors via HTTPS for given repo
 //cb = callback function to handle asynchronous nature of results that get returned from getRepoContributors
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
