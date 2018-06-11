f1b5c0a829d440f47473058cd597a5cbaf9a8401
Tiffany <tiffanyjdow@gmail.com>
2018-06-11 20:07:47 +0000

    Added getRepoContributors function declaration with arguments

--- download_avatars.js
+++ download_avatars.js
@@ -1,3 +1,14 @@
 var request = require('request');
 
 console.log("Welcome to the GitHub Avatar Downloader");
+
+function getRepoContributors(repoOwner, repoName, cb) {
+
+}
+
+getRepoContributors("jquery", "jquery", function(err, result) {
+  console.log("Errors:", err);
+  console.log("Results:", result);
+});
+//function to use request lib to fetch list of contributors via HTTPS for given repo
+//cb = callback function to handle asynchronous nature of results that get returned from getRepoContributors
