7dd4b54bf66684e23a7aafdd1e3605ed418bd2bd
Tiffany <tiffanyjdow@gmail.com>
2018-06-11 22:19:40 +0000

    added to function to pass in users URL's

--- download_avatars.js
+++ download_avatars.js
@@ -1,4 +1,5 @@
 var request = require('request');
+var fs = require('fs');
 var include = ('./secrets.js');
 
 
@@ -10,21 +11,65 @@ function getRepoContributors(repoOwner, repoName, cb) {
     url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
     headers: {
       'User-Agent': 'request',
-      'Authorization': 'GITHUB_TOKEN'
+      'Authorization': include.GITHUB_TOKEN
     }
   };
 
   request(options, function(err, res, body) {
-    cb(err, body);
+    let parsedBody = JSON.parse(body);
+    //cb(err, parsIntResult[0].avatar_url);
+
+    for(i = 0; i < parsedBody.length; i++) {
+      //var avatar_url = res.length[i].avatar_url;
+      cb(err, parsedBody[i].avatar_url);
+    }
   });
 }
 
 getRepoContributors("jquery", "jquery", function(err, result) {
-  console.log("Errors:", err);
+  //console.log("Errors:", err);
   console.log("Results:", result);
+  //return result;
 });
-//function to use request lib to fetch list of contributors via HTTPS for given repo
-//cb = callback function to handle asynchronous nature of results that get returned from getRepoContributors
+
+
+// function downloadImageByURL (url, filePath) {
+
+//   request.get(url).on('error', function(err) {
+//     throw err;
+//   })
+//   .on('response', function (response) {
+//     console.log('Response Status Code: ', response.statusCode);
+//     console.log(response.headers['content-type']);
+//     console.log('Downloading image...');
+//     console.log('Download complete.');
+//   })
+//   .pipe(fs.createWriteStream(filePath));
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
